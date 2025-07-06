3fc5ea9d898aaab0451a2cd4f747dabb01cd5857
jiaqijingliu <429712080@qq.com>
2025-07-06 22:06:16 +0800

    Site updated: 2025-07-06 22:06:13

--- js/matery.js
+++ js/matery.js
@@ -170,3 +170,8 @@ $(function () {
     // 初始化加载 tooltipped.
     $('.tooltipped').tooltip();
 });
+
+hexo.extend.filter.register('theme_inject', function (injects) {
+  // 引入 APlayer
+  injects.head.file('aplayer', 'source/_data/APlayer.swig', {aplayer: hexo.theme.config.aplayer});
+});
